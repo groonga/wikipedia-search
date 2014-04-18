d7837d22f961a40ff2bb7126c3ea1eacdad0dc72
YUKI Hiroshi <shimoda@clear-code.com>
2014-04-18 13:30:29 +0900

    Add generated cookbook files

---
+++ chef/site-cookbooks/wikipedia-search/metadata.rb
@@ -0,0 +1,7 @@
+name             'wikipedia-search'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures wikipedia-search'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ chef/site-cookbooks/wikipedia-search/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: wikipedia-search
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "droonga::engine"
