d4347d52e2ecf517801c624c95db223a471c050a
Kouhei Sutou <kou@clear-code.com>
2015-05-17 18:18:24 +0900

    Use one INSERT for one page

--- lib/wikipedia-search/sql-converter.rb
+++ lib/wikipedia-search/sql-converter.rb
@@ -4,10 +4,47 @@ module WikipediaSearch
   class SQLConverter < Converter
     private
     def create_listener(output)
-      SQLListener.new(output, @options)
+      SQLOneShotListener.new(output, @options)
+      # SQLBulkListener.new(output, @options)
     end
 
     class SQLListener < Listener
+      private
+      def escape_string(string)
+        escaped_content = string.gsub(/['\\\n]/) do |special_character|
+          case special_character
+          when "\n"
+            "\\n"
+          when "'"
+            "''"
+          else
+            "\\#{special_character}"
+          end
+        end
+        "'#{escaped_content}'"
+      end
+    end
+
+    class SQLOneShotListener < SQLListener
+      private
+      def on_start
+      end
+
+      def on_finish
+      end
+
+      def on_page(page)
+        record_values = [
+          @page.id,
+          escape_string(@page.title),
+          escape_string(shorten_text(@page.text)),
+        ]
+        @output.print("INSERT INTO wikipedia (id, title, text) VALUES ")
+        @output.print("(#{record_values.join(', ')});")
+      end
+    end
+
+    class SQLBulkListener < SQLListener
       private
       def on_start
         @output.puts("INSERT INTO wikipedia (id, title, text) VALUES")
@@ -26,20 +63,6 @@ module WikipediaSearch
           escape_string(shorten_text(@page.text)),
         ]
         @output.print("(#{record_values.join(', ')})")
-      end
-
-      def escape_string(string)
-        escaped_content = string.gsub(/['\\\n]/) do |special_character|
-          case special_character
-          when "\n"
-            "\\n"
-          when "'"
-            "''"
-          else
-            "\\#{special_character}"
-          end
-        end
-        "'#{escaped_content}'"
       end
     end
   end
