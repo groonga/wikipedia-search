c56821748fafa509e6131256d35e15ec315cfdef
Kouhei Sutou <kou@clear-code.com>
2016-11-07 21:59:25 +0900

    Reduce the max N characters to 1000000
    Because the following error was returned:
    
        string is too long for tsvector (1618908 bytes, max 1048575 bytes)

--- benchmark/centos7/postgresql.sh
+++ benchmark/centos7/postgresql.sh
@@ -461,7 +461,7 @@ benchmark_search_textsearch()
     commands+=("--command" "${enable_seqscan}")
     commands+=("--command" "\\timing")
     for i in $(seq ${n_search_tries}); do
-      target="to_tsvector('english', text)"
+      target="to_tsvector('english', substring(text from 0 for 1000000))"
       query="to_tsquery('english', '$(echo ${search_word} | sed -e 's/ OR / | /g')')"
       where="${target} @@ ${query}"
       commands+=("--command" "SELECT COUNT(*) FROM wikipedia WHERE ${where}")
--- config/sql/indexes.textsearch.sql
+++ config/sql/indexes.textsearch.sql
@@ -1,2 +1,3 @@
 CREATE INDEX wikipedia_index_textsearch ON wikipedia
- USING GIN (to_tsvector('english', title), to_tsvector('english', text));
+ USING GIN (to_tsvector('english', title),
+            to_tsvector('english', substring(text from 0 for 1000000)));
